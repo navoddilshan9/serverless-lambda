dc607e99a7d4eb77602cf57f1a1c11d880b65dcb
Navod Dilshan <navoddilshan9@gmail.com>
2022-11-06 14:12:02 +0530

    test

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,18 @@
 ;('use strict')
 var connectToDatabase = require('../config/dbConfig')
 var Post = require('./collections/post')
+
 module.exports.hello = async (event) => {
-  const dbConnection = connectToDatabase()
-  var id = '63675c843777f6bc82d8765e'
-  const post = await Post.findOne()
   return {
     statusCode: 200,
-    body: JSON.stringify(post),
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: 'event',
+      },
+      null,
+      2
+    ),
   }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
