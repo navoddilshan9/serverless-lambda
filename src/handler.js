e7eb54f6af216b84a9655fcd24607ee0c6343a88
Navod Dilshan <navoddilshan9@gmail.com>
2022-11-06 14:26:00 +0530

    test

--- src/handler.js
+++ src/handler.js
@@ -18,7 +18,7 @@ var Post = require('./collections/post')
 module.exports.hello = async (event) => {
   const dbConnection = connectToDatabase()
   var id = '63675c843777f6bc82d8765e'
-  const post = await Post.findOne()
+  const post = await Post.find()
   return {
     statusCode: 200,
     body: JSON.stringify(post),
