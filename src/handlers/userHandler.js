6b84fc5f0adb61dcf58242497070ed2d68a8409e
Navod Dilshan <navoddilshan9@gmail.com>
2022-11-13 13:50:12 +0530

    testing

--- src/handlers/userHandler.js
+++ src/handlers/userHandler.js
@@ -14,29 +14,11 @@ const saveUser = async (userAttributes) => {
   console.log(userAttributes['email'])
   console.log(userAttributes.email)
   await User.findById({
-    email: userAttributes['email'],
+    email: userAttributes.email,
   })
     .then(async (currentUser) => {
       console.log('currentUser')
       console.log(currentUser)
-      if (currentUser) {
-        console.log('User existing')
-      } else {
-        const user = new User({
-          firstName: userAttributes['custom:firstName'],
-          lastName: userAttributes['custom:lastName'],
-          email: userAttributes['email'],
-        })
-        await user
-          .save()
-          .then((user) => {
-            console.log('new user added')
-            console.log(user)
-          })
-          .catch((err) => {
-            console.log(err)
-          })
-      }
     })
     .catch((err) => {
       console.log(err)
