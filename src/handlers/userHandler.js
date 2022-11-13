76be4646f39be33bd9ddfa0e352ddab2c5d89147
Navod Dilshan <navoddilshan9@gmail.com>
2022-11-13 10:49:57 +0530

    testing

--- src/handlers/userHandler.js
+++ src/handlers/userHandler.js
@@ -5,44 +5,40 @@ var User = require('../collections/user')
 module.exports.create = async (event, context, callback) => {
   await connectDB()
   console.log('body')
-  console.log(event)
-  const user = new User(event)
-  let body = {
-    statusCode: 400,
-    body: JSON.stringify(
-      {
-        message: 'User cannot create',
-      },
-      null,
-      2
-    ),
-  }
-  await user
-    .save()
-    .then((user) => {
-      body = {
-        statusCode: 200,
-        body: JSON.stringify(
-          {
-            message: user,
-          },
-          null,
-          2
-        ),
+  console.log(event.request.userAttributes)
+  saveUser(event.request.userAttributes)
+  callback(null, event)
+}
+
+const saveUser = async (userAttributes) => {
+  const values = Object.values(userAttributes)
+
+  await User.findById({
+    email: values[2],
+  })
+    .then(async (currentUser) => {
+      console.log('currentUser')
+      console.log(currentUser)
+      if (currentUser) {
+        console.log('User existing')
+      } else {
+        const user = new User({
+          firstName: values[0],
+          lastName: values[1],
+          email: values[2],
+        })
+        await user
+          .save()
+          .then((user) => {
+            console.log('new user added')
+            console.log(user)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
       }
     })
     .catch((err) => {
-      body = {
-        statusCode: 400,
-        body: JSON.stringify(
-          {
-            message: 'user cannot created!',
-            error: err,
-          },
-          null,
-          2
-        ),
-      }
+      console.log(err)
     })
-  callback(null, event)
 }
