01001cda9c365e50cf8953a341ce8b70995b9689
Navod Dilshan <navoddilshan9@gmail.com>
2022-11-13 09:48:06 +0530

    testing

--- src/handlers/userHandler.js
+++ src/handlers/userHandler.js
@@ -6,6 +6,7 @@ module.exports.create = async (event, context, callback) => {
   await connectDB()
   console.log('body')
   console.log(event)
+  const user = new User(event)
   let body = {
     statusCode: 400,
     body: JSON.stringify(
@@ -16,6 +17,33 @@ module.exports.create = async (event, context, callback) => {
       2
     ),
   }
+  await user
+    .save()
+    .then((user) => {
+      body = {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: user,
+          },
+          null,
+          2
+        ),
+      }
+    })
+    .catch((err) => {
+      body = {
+        statusCode: 400,
+        body: JSON.stringify(
+          {
+            message: 'user cannot created!',
+            error: err,
+          },
+          null,
+          2
+        ),
+      }
+    })
   callback(null, event)
-  return event
+  return body
 }
