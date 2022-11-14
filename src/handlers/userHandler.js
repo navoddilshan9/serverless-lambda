1a014da69852540e3436f79d77177c657e2e15b3
Navod Dilshan <navoddilshan9@gmail.com>
2022-11-14 17:01:54 +0530

    testing

--- src/handlers/userHandler.js
+++ src/handlers/userHandler.js
@@ -3,29 +3,31 @@ var connectDB = require('../../config/dbConfig')
 var User = require('../collections/user')
 
 module.exports.create = async (event, context, callback) => {
-  let userAttributes = event.request.userAttributes
-  await connectDB()
-  await User.find({
-    email: userAttributes.email,
-  })
-    .then(async (currentUser) => {
-      if (currentUser.length === 0) {
-        const user = new User({
-          firstName: userAttributes['custom:firstName'],
-          lastName: userAttributes['custom:lastName'],
-          email: userAttributes.email,
-        })
-        user.save().then((nuser) => {
-          console.log(nuser)
-          callback(null, event)
-        })
-      } else {
-        console.log('user existing')
-        callback(null, event)
-      }
-    })
-    .catch((err) => {
-      console.log(err)
-      callback(null, event)
-    })
+  // let userAttributes = event.request.userAttributes
+  // await connectDB()
+  // await User.find({
+  //   email: userAttributes.email,
+  // })
+  //   .then(async (currentUser) => {
+  //     if (currentUser.length === 0) {
+  //       const user = new User({
+  //         firstName: userAttributes['custom:firstName'],
+  //         lastName: userAttributes['custom:lastName'],
+  //         email: userAttributes.email,
+  //       })
+  //       user.save().then((nuser) => {
+  //         console.log(nuser)
+  //         callback(null, event)
+  //       })
+  //     } else {
+  //       console.log('user existing')
+  //       callback(null, event)
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //     callback(null, event)
+  //   })
+
+  callback(null, event)
 }
